9105daa88f3e4558c3c8367cf3af8271a0bc6397
Timothy Baldock <tb@entropy.me.uk>
2011-04-14 23:38:24 +0000

    Add licence.txt Update translation strings Update todo Add build script to automatically produce distributable versions of addon
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/sixornot/trunk@620 741c516a-3ba6-4fa8-b112-5c51f26a88df

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This script builds the SixOrNot addon ready for distribution
+version="0.1"
+
+# Create build directory by version
+mkdir ../dist/$version
+
+# Copy distribution files into build directory
+mkdir ../dist/$version/src
+cp licence.txt ../dist/$version/src/
+cp bootstrap.js ../dist/$version/src/
+cp install.rdf ../dist/$version/src/
+cp icon.png ../dist/$version/src/
+cp icon64.png ../dist/$version/src/
+mkdir ../dist/$version/src/images
+cp images/*.png ../dist/$version/src/images/
+mkdir ../dist/$version/src/includes
+cp includes/locale.js ../dist/$version/src/includes/
+cp includes/utils.js ../dist/$version/src/includes/
+
+# Copy locale(s)
+mkdir ../dist/$version/src/locale
+mkdir ../dist/$version/src/locale/en-GB
+cp locale/en-GB/sixornot.properties ../dist/$version/src/locale/en-GB/
+
+# Create .zip file from build directory (with .xpi file extension)
+cd ../dist/$version/src/
+zip -r ../sixornot.xpi *
