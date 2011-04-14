41159240375da9cc35f3ad61de5e4ff3fe45942c
Timothy Baldock <tb@entropy.me.uk>
2011-04-14 11:14:36 +0000

    Add image icons in better location Start to move code into bootstrap.js similar to the html5 addon Add utility and locale functions from html5 addon
    git-svn-id: svn+ssh://entropy.me.uk/var/svn/timothy/sixornot/trunk@614 741c516a-3ba6-4fa8-b112-5c51f26a88df

--- bootstrap.js
+++ bootstrap.js
@@ -15,7 +15,7 @@
     onWindowTitleChange: function(aWindow, aTitle) { }
 }*/
 
-function windowWatcher(subject, topic) {
+/*function windowWatcher(subject, topic) {
     if (topic != "domwindowopened") return;
 
     subject.addEventListener("load", function () {
@@ -28,12 +28,153 @@ function windowWatcher(subject, topic) {
             Sixornot.init(subject);
         }
     }, false);
+} */
+
+
+/*
+    Constants and global variables
+*/
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+Cu.import("resource://gre/modules/AddonManager.jsm");
+
+const NS_XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
+const BUTTON_ID     = "sixornot-buttonid",
+      ADDRESSBAR_ID = "sixornot-addressbarid",
+      PREF_TOOLBAR  = "toolbar",
+      PREF_NEXTITEM = "nextitem";
+
+const PREF_BRANCH_SIXORNOT = Services.prefs.getBranch("extensions.sixornot.");
+
+const PREFS = {
+   enable:    false,
+   nextitem:  "bookmarks-menu-button-container",
+   toolbar:   "nav-bar"
+};
+
+let PREF_OBSERVER = {
+  observe: function(aSubject, aTopic, aData) {
+    if ("nsPref:changed" != aTopic || !PREFS[aData]) return;
+    runOnWindows(function(win) {
+      win.document.getElementById(KEY_ID).setAttribute(aData, getPref(aData));
+      addMenuItem(win);
+    });
+  }
 }
 
 /*
- bootstrap.js API
+    ipv6 only                   6only_16.png, 6only_24.png
+    ipv4+ipv6 w/ local ipv6     6and4_16.png, 6and4_24.png
+    ipv4+ipv6 w/o local ipv6    4pot6_16.png, 4pot6_24.png
+    ipv4 only                   4only_16.png, 4only_24.png
+    Unknown                     other_16.png, other_24.png
 */
-function startup(aData, aReason) {
+let s6only_16 = "", s6and4_16 = "", s4pot6_16 = "", s4only_16 = "", sother_16 = "";
+let s6only_24 = "", s6and4_24 = "", s4pot6_24 = "", s4only_24 = "", sother_24 = "";
+
+(function(global) global.include = function include(src) (
+    Services.scriptloader.loadSubScript(src, global)))(this);
+
+/*
+    Core functionality
+*/
+function main(win) {
+    consoleService.logStringMessage("Sixornot - main");
+    let doc = win.document;
+
+    // Add iconized button
+    let (toggleButton = doc.createElementNS(NS_XUL, "toolbarbutton")) {
+        toggleButton.setAttribute("id", BUTTON_ID);
+        toggleButton.setAttribute("label", getLocalizedStr("label"));
+        toggleButton.setAttribute("class", "toolbarbutton-1 chromeclass-toolbar-additional");
+
+        toggleButton.setAttribute("tooltiptext", getLocalizedStr("tt_default"));
+        toggleButton.style.listStyleImage = "url('" + sother_16 + "')";
+
+//        toggleButton.addEventListener("command", toggle, true);
+        $(doc, "navigator-toolbox").palette.appendChild(toggleButton);
+ 
+        // Move to location specified in prefs
+        let toolbarId = PREF_BRANCH_SIXORNOT.getCharPref(PREF_TOOLBAR);
+        let toolbar = toolbarId && $(doc, toolbarId);
+        if (toolbar) {
+            let nextItem = $(doc, PREF_BRANCH_SIXORNOT.getCharPref(PREF_NEXTITEM));
+            toolbar.insertItem(BUTTON_ID, nextItem && nextItem.parentNode.id == toolbarId && nextItem);
+        }
+
+        win.addEventListener("aftercustomization", toggleCustomize, false);
+    }
+
+    // Add address bar icon
+
+    unload(function() {
+        let button = $(doc, BUTTON_ID) || $($(doc,"navigator-toolbox").palette, BUTTON_ID);
+        button && button.parentNode.removeChild(button);
+
+        win.removeEventListener("aftercustomization", toggleCustomize, false);
+    }, win);
+}
+
+
+
+/*
+    bootstrap.js API
+*/
+// NEW
+function startup(data) AddonManager.getAddonByID(data.id, function(addon) {
+    consoleService.logStringMessage("Sixornot - setInitialPrefs");
+    setInitialPrefs();
+    include(addon.getResourceURI("includes/utils.js").spec);
+    include(addon.getResourceURI("includes/locale.js").spec);
+
+    initLocalization(addon, "sixornot.properties");
+    s6only_16 = addon.getResourceURI("images/6only_16.png").spec;
+    s6and4_16 = addon.getResourceURI("images/6and4_16.png").spec;
+    s4pot6_16 = addon.getResourceURI("images/4pot6_16.png").spec;
+    s4only_16 = addon.getResourceURI("images/4only_16.png").spec;
+    sother_16 = addon.getResourceURI("images/other_16.png").spec;
+    s6only_24 = addon.getResourceURI("images/6only_24.png").spec;
+    s6and4_24 = addon.getResourceURI("images/6and4_24.png").spec;
+    s4pot6_24 = addon.getResourceURI("images/4pot6_24.png").spec;
+    s4only_24 = addon.getResourceURI("images/4only_24.png").spec;
+    sother_24 = addon.getResourceURI("images/other_24.png").spec;
+
+    watchWindows(main);
+
+    let prefs = PREF_BRANCH_SIXORNOT;
+    prefs = prefs.QueryInterface(Components.interfaces.nsIPrefBranch2);
+    prefs.addObserver("", PREF_OBSERVER, false);
+
+    unload(function() prefs.removeObserver("", PREF_OBSERVER));
+});
+
+// NEW
+function shutdown(data, reason) {
+    consoleService.logStringMessage("Sixornot - shutdown");
+    if (reason !== APP_SHUTDOWN) unload();
+}
+
+// NEW
+function install(){
+    consoleService.logStringMessage("Sixornot - install");
+    setInitialPrefs();
+}
+
+// NEW
+function uninstall(){
+    consoleService.logStringMessage("Sixornot - uninstall");
+// If this is due to an upgrade then don't delete preferences?
+// Some kind of upgrade function to potentially upgrade preference settings may be required
+//   let value = PREF_BRANCH_HTML5TOGGLE.getBoolPref("enable");
+    PREF_BRANCH_SIXORNOT.deleteBranch("");             
+//   PREF_BRANCH_HTML5.setBoolPref('enable', value);
+}
+
+
+
+
+// OLD
+/*function startup(aData, aReason) {
     Components.utils.import("resource://gre/modules/Services.jsm");
 
     // Set up access to resource: URIs
@@ -62,9 +203,10 @@ function startup(aData, aReason) {
 
     // Load into any new windows
     Services.ww.registerNotification(windowWatcher);
-}
+} */
 
-function shutdown(aData, aReason) {
+// OLD
+/*function shutdown(aData, aReason) {
     // When the application is shutting down we normally don't have to clean up any UI changes
     if (aReason == APP_SHUTDOWN) return;
 
@@ -85,5 +227,82 @@ function shutdown(aData, aReason) {
 
 function install(aData, aReason) { }
 
-function uninstall(aData, aReason) { }
+function uninstall(aData, aReason) { } */
+
+
+/*
+    Utility functions
+*/
+
+// Update preference which determines location of button when loading into new windows
+function toggleCustomize(event) {
+    consoleService.logStringMessage("Sixornot - toggleCustomize");
+   let toolbox = event.target, toolbarId, nextItemId;
+   let button = $(toolbox.parentNode, BUTTON_ID);
+   if (button) {
+      let parent = button.parentNode,
+          nextItem = button.nextSibling;
+      if (parent && parent.localName == "toolbar") {
+          toolbarId = parent.id;
+          nextItemId = nextItem && nextItem.id;
+      }
+   }
+   PREF_BRANCH_SIXORNOT.setCharPref(PREF_TOOLBAR,  toolbarId || "");
+   PREF_BRANCH_SIXORNOT.setCharPref(PREF_NEXTITEM, nextItemId || "");
+}
+
+// Return preference value, either from prefs store or from internal defaults
+function getPref(name) {
+    consoleService.logStringMessage("Sixornot - getPref");
+   try {
+      return PREF_BRANCH_SIXORNOT.getComplexValue(name, Ci.nsISupportsString).data;
+   } catch(e){}
+   return PREFS[name];
+}
+
+// Proxy to getElementById
+function $(node, childId) {
+   if (node.getElementById) {
+      return node.getElementById(childId);
+   } else {
+      return node.querySelector("#" + childId);
+   }
+}
+
+// Set up initial values for preferences
+function setInitialPrefs() {
+    consoleService.logStringMessage("Sixornot - setInitialPrefs");
+   let branch = PREF_BRANCH_SIXORNOT;
+   for (let [key, val] in Iterator(PREFS)) {
+      switch (typeof val) {
+         case "boolean":
+            branch.setBoolPref(key, val);
+            break;
+         case "number":
+            branch.setIntPref(key, val);
+            break;
+         case "string":
+            branch.setCharPref(key, val);
+            break;
+      }
+   }
+
+   // save the current value of the html5.enable preference
+//   let value = PREF_BRANCH_HTML5.getBoolPref("enable");
+//   PREF_BRANCH_HTML5TOGGLE.setBoolPref('enable', value);
+}
+
+// Lazy getter services
+function defineLazyGetter(getterName, getterFunction)
+{
+    this.__defineGetter__(getterName, function() {
+        delete this[getterName];
+        return this[getterName] = getterFunction.apply(this);
+    });
+}
+
+defineLazyGetter("consoleService", function() {
+    return Components.classes["@mozilla.org/consoleservice;1"]
+                     .getService(Components.interfaces.nsIConsoleService);
+});
 
